f1b43e0371ea65db20b7d0eac7239e72c5e40617
awsh-code <108316895+awsh-code@users.noreply.github.com>
2023-02-01 08:56:30 -0500

    Initial commit

---
+++ src/qapi.js
@@ -0,0 +1,29 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"63da6debf0019100228edc04",
+				"63da6debf0019100228edc07"
+			]
+		},
+		"63da6debf0019100228edc04": {
+			"id": "63da6debf0019100228edc04",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"63da6debf0019100228edc07": {
+			"id": "63da6debf0019100228edc07",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "create-react-app",
+	"site": {
+		"styles": {},
+		"seo": {}
+	}
+}
